24c7f873aa84449a8cb789817266f4d84fa9303c
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2023-08-04 19:32:41 +0300

    hw task one and two

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulGallery = document.querySelector('.gallery')
+ulGallery.addEventListener("click", onOpenModalWindowClick)
+
+const markupGallery = ({ preview, original, description }) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+
+const listImg = galleryItems.reduce((acc, elem) => acc + markupGallery(elem),[])
+ulGallery.insertAdjacentHTML('beforeend', listImg)
+
+function onOpenModalWindowClick(event) {
+  event.preventDefault()
+    if (event.target.src === event.target.dataset.source) {
+        return
+    }
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src='${event.target.src}' width="600" height="400">
+    </div>
+`)
+    instance.show()
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const ulGallery = document.querySelector('.gallery')
+ulGallery.addEventListener('click', onOpenSliderWIndowClicl)
+
+const markupGallery = ({ preview, original, description }) => 
+`<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+   </a>
+</li>`.join('')
+
+const downloadMarkupGallery = galleryItems.reduce((acc, elem) => acc + markupGallery(elem), [])
+ulGallery.insertAdjacentHTML("beforeend", downloadMarkupGallery)
+
+function onOpenSliderWIndowClicl() {
+    
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+})
