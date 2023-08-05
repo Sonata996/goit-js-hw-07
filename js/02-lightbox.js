8cd5847932a91a867335f94ba442d7750137f7af
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2023-08-05 20:58:52 +0300

    d

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,22 +5,22 @@ console.log(galleryItems);
 
 
 const ulGallery = document.querySelector('.gallery')
-ulGallery.addEventListener('click', onOpenSliderWIndowClicl)
+// ulGallery.addEventListener('click', onOpenSliderWIndowClicl)
 
 const markupGallery = ({ preview, original, description }) => 
 `<li class="gallery__item">
    <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}"/>
    </a>
-</li>`.join('')
+</li>`
 
 const downloadMarkupGallery = galleryItems.reduce((acc, elem) => acc + markupGallery(elem), [])
 ulGallery.insertAdjacentHTML("beforeend", downloadMarkupGallery)
 
-function onOpenSliderWIndowClicl() {
+// function onOpenSliderWIndowClicl() {
     
-}
+// }
 const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250
+   //  captionsData: 'alt',
+   //  captionDelay: 250
 })
